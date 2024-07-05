9fe68e7190d70ccd0aeb13e4b04892ba69d47b28
Nihal <nihalmujawar28413@gmail.com>
2024-07-05 21:27:14 +0530

    added async function

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,18 @@
 let api_key = "574e4c5c2052e45744593b504f4b09e3";
 let input = document.querySelector("input");
 let button = document.querySelector("button");
+
+button.addEventListener("click", () => {
+  let inputValue = input.value.trim();
+
+  if (!inputValue) {
+    alert(`Provide the city`);
+  } else {
+    fetchWeather(inputValue);
+  }
+});
+
+
+const weather = async (city) =>{
+  
+}
