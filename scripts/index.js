31863d3d72496ef2c6e0bf49b7a7955f52ef3aa0
Nihal <nihalmujawar28413@gmail.com>
2024-07-05 21:39:43 +0530

    completed the async() function

--- scripts/index.js
+++ scripts/index.js
@@ -8,11 +8,21 @@ button.addEventListener("click", () => {
   if (!inputValue) {
     alert(`Provide the city`);
   } else {
-    fetchWeather(inputValue);
+    weather(inputValue);
   }
 });
 
-
-const weather = async (city) =>{
-  
-}
+const weather = async (city) => {
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${api_key}&units=metric`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      console.error("error while fetching data");
+    } else {
+      const data = await response.json();
+      console.log(data);
+    }
+  } catch (error) {
+    console.error("Error", error);
+  }
+};
